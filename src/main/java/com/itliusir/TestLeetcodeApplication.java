5a6e435c6cd00fb0157f10cd930843f10d0c098e
liugang <liugang5@ifeng.com>
2019-07-19 19:31:56 +0800

    add leetcode_2

---
+++ src/main/java/com/itliusir/TestLeetcodeApplication.java
@@ -0,0 +1,20 @@
+package com.itliusir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * leetcode problem solution project
+ *
+ * @author liugang
+ * @since 2019/7/18
+ */
+@SpringBootApplication
+public class TestLeetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestLeetcodeApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/itliusir/array/AddTwoNumbers.java
@@ -0,0 +1,71 @@
+package com.itliusir.array;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 给出两个 非空 的链表用来表示两个非负的整数。其中，它们各自的位数是按照 逆序 的方式存储的，并且它们的每个节点只能存储 一位 数字。
+ * 如果，我们将这两个数相加起来，则会返回一个新的链表来表示它们的和。
+ * 您可以假设除了数字 0 之外，这两个数都不会以 0 开头。
+ * 示例：
+ * <p>
+ * 输入：(2 -> 4 -> 3) + (5 -> 6 -> 4)
+ * 输出：7 -> 0 -> 8
+ * 原因：342 + 465 = 807
+ *
+ * @author liugang
+ * @since 2019/7/18
+ */
+
+@Slf4j
+public class AddTwoNumbers {
+
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2);
+        ListNode l11 = new ListNode(4);
+        ListNode l111 = new ListNode(3);
+        l1.next = l11;
+        l11.next = l111;
+
+        ListNode l2 = new ListNode(5);
+        ListNode l22 = new ListNode(6);
+        ListNode l222 = new ListNode(4);
+        l2.next = l22;
+        l22.next = l222;
+
+        log.info("addTwoNumbers result : {}", addTwoNumbers(l1, l2));
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode root = new ListNode(0);
+        int carry = 0;
+        ListNode curNode = root;
+        while (l1 != null || l2 != null || carry != 0) {
+            int x = l1 != null ? l1.val : 0;
+            int y = l2 != null ? l2.val : 0;
+            int sum = x + y + carry;
+            carry = sum / 10;
+            ListNode sumNode = new ListNode(sum % 10);
+            curNode.next = sumNode;
+            curNode = sumNode;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return root.next;
+    }
+
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+}
