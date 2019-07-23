2cfbd0077089aa825c8ade0ccf850ffc9f77aa02
liugang <liugang5@ifeng.com>
2019-07-23 22:45:06 +0800

    add problem seq

--- src/main/java/com/itliusir/array/KthLargest.java
+++ src/main/java/com/itliusir/array/KthLargest.java
@@ -2,6 +2,26 @@ package com.itliusir.array;
 
 import java.util.PriorityQueue;
 
+
+/**
+ * problem.703
+ * 设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。
+ * 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。
+
+
+ * 示例:
+
+ int k = 3;
+ int[] arr = [4,5,8,2];
+ KthLargest kthLargest = new KthLargest(3, arr);
+ kthLargest.add(3);   // returns 4
+ kthLargest.add(5);   // returns 5
+ kthLargest.add(10);  // returns 5
+ kthLargest.add(9);   // returns 8
+ kthLargest.add(4);   // returns 8
+ *
+ * @author liugang 2019/7/23
+ * */
 public class KthLargest {
 
     final PriorityQueue<Integer> q;
@@ -26,9 +46,13 @@ public class KthLargest {
     }
 
     public static void main(String[] args) {
-        int[] a = {2, 3, 4, 5, 6, 3, 2, 1};
-        KthLargest kthLargest = new KthLargest(7, a);
-        int result = kthLargest.add(7);
-        System.out.println(result);
+        int[] arr = {4,5,8,2};
+        int k = 3;
+        KthLargest kthLargest = new KthLargest(k, arr);
+        System.out.println(kthLargest.add(3));
+        System.out.println(kthLargest.add(5));
+        System.out.println(kthLargest.add(10));
+        System.out.println(kthLargest.add(9));
+        System.out.println(kthLargest.add(4));
     }
 }
