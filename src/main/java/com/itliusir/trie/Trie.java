de188c1795091c777aae5634fb45ea7f122ba8fb
liugang <liugang5@ifeng.com>
2019-10-19 10:28:14 +0800

    add problem 283 11

---
+++ src/main/java/com/itliusir/trie/Trie.java
@@ -0,0 +1,55 @@
+/*
+package com.itliusir.trie;
+
+*/
+/**
+ * problem.208
+ *
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ *
+ * @author liugang
+ * @since 2019/9/11
+ *//*
+
+public class Trie {
+
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+
+        trie.insert("apple");
+        trie.search("apple");
+        trie.search("app");
+        trie.startsWith("app");
+        trie.insert("app");
+        trie.search("app");
+    }
+
+    */
+/** Initialize your data structure here. *//*
+
+    public Trie() {
+
+    }
+
+    */
+/** Inserts a word into the trie. *//*
+
+    public void insert(String word) {
+
+    }
+
+    */
+/** Returns if the word is in the trie. *//*
+
+    public boolean search(String word) {
+
+    }
+
+    */
+/** Returns if there is any word in the trie that starts with the given prefix. *//*
+
+    public boolean startsWith(String prefix) {
+
+    }
+}
+*/
---
+++ src/main/java/com/itliusir/util/PrintArrayUtil.java
@@ -0,0 +1,16 @@
+package com.itliusir.util;
+
+/**
+ * 打印数组工具类
+ *
+ * @author liugang
+ * @since 2019/10/18
+ */
+public class PrintArrayUtil {
+
+    public static void printArray(int[] nums) {
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+    }
+}
