09e7c96a0a8b6584a40cb22097b7b2e3beed45f3
liugang <liugang5@ifeng.com>
2019-10-25 18:14:04 +0800

    add problem 94

---
+++ src/main/java/com/itliusir/util/TreeNode.java
@@ -0,0 +1,14 @@
+package com.itliusir.util;
+
+/**
+ * 树节点
+ *
+ * @author liugang
+ * @since 2019/10/25
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+}
